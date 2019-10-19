21781f49b169946092cd55e0132b141d60d0fa96
Sayak Banerjee <sayakani1998@gmail.com>
2019-10-19 20:31:56 +0530

    Commit New1

--- app/src/main/java/com/example/android/smartparking/MainActivity.java
+++ app/src/main/java/com/example/android/smartparking/MainActivity.java
@@ -46,12 +46,43 @@ public class MainActivity extends AppCompatActivity {
                 String value=text.getText().toString();
                 EditText text1=findViewById(R.id.phone_number);
                 String value1=text1.getText().toString();
+                EditText text2=findViewById(R.id.email_id);
+                String value2=text2.getText().toString();
+                final TextView textView = findViewById(R.id.index);
+                final TextView textView1 = findViewById(R.id.index1);
+
+
+                final RadioGroup radioGroup = findViewById(R.id.radio);
+                final RadioGroup radioGroup1 = findViewById(R.id.radio1);
+                final RadioGroup radioGroup2 = findViewById(R.id.ampm);
+                int checkedRadio = radioGroup.getCheckedRadioButtonId();
+                RadioButton checkedRadioButton = findViewById(checkedRadio);
+                String checkedBox = checkedRadioButton.getText().toString();
+                int checkedRadio1 = radioGroup1.getCheckedRadioButtonId();
+                RadioButton checkedRadioButton1 = findViewById(checkedRadio1);
+                String checkedBox1 = checkedRadioButton1.getText().toString();
+                int checkedRadio2 = radioGroup2.getCheckedRadioButtonId();
+                RadioButton checkedRadioButton2 = findViewById(checkedRadio2);
+                String checkedBox2 = checkedRadioButton2.getText().toString();
+
+                String hh = Integer.toString(quantity1);
+                String mm = Integer.toString(quantity);
+
                 String car=value;
                 String ph=value1;
+                String em=value2;
+                String vty=checkedBox1;
+                String loca=checkedBox;
                 member.setCarno(car);
                 member.setPhno(ph);
+                member.setEmail(em);
+                member.setVt(vty);
+                member.setLoc(loca);
+                member.setHour(hh);
+                member.setMinute(mm);
+                member.setUnit(checkedBox2);
                 reff.push().setValue(member);
-                Toast.makeText(MainActivity.this,"data succesful",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"Your Booking Will be Confirmed Soon",Toast.LENGTH_LONG).show();
 
             }
         });
@@ -98,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_TEXT, "Team Park It Right,\n\n"+priceMessage+"\n");
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-
         }
     }
 
--- app/src/main/java/com/example/android/smartparking/Member.java
+++ app/src/main/java/com/example/android/smartparking/Member.java
@@ -3,6 +3,12 @@ package com.example.android.smartparking;
 public class Member {
     private String Carno;
     private String Phno;
+    private String email;
+    private String vt;
+    private String loc;
+    private String hour;
+    private String minute;
+    private String unit;
 
 
     public Member() {
@@ -23,5 +29,53 @@ public class Member {
 
     public void setPhno(String phno) {
         Phno = phno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVt() {
+        return vt;
+    }
+
+    public void setVt(String vt) {
+        this.vt = vt;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
 }
