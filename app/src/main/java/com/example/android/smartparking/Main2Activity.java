c64d67fcdd752b0f49f2540b8f79b722881f47e0
Sayak Banerjee <sayakani1998@gmail.com>
2019-10-19 22:37:30 +0530

    Commit New1 Final

--- app/src/main/java/com/example/android/smartparking/Main2Activity.java
+++ app/src/main/java/com/example/android/smartparking/Main2Activity.java
@@ -17,6 +17,7 @@ public class Main2Activity extends AppCompatActivity {
     // Add button Move to Activity
 
     Button next_Activity_button;
+    Button next_Activity_button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,6 +30,7 @@ public class Main2Activity extends AppCompatActivity {
         // use findViewById() to get the Button
         next_Activity_button = (Button)findViewById(R.id.first_activity_button);
         question1 = (TextView)findViewById(R.id.question1_id);
+        next_Activity_button1 = (Button)findViewById(R.id.second_activity_button);
 
         // In question1 get the TextView use by findViewById()
         // In TextView set question Answer for message
@@ -51,5 +53,20 @@ public class Main2Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        next_Activity_button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View x)
+        {
+
+            // Intents are objects of the android.content.Intent type. Your code can send them
+            // to the Android system defining the components you are targeting.
+            // Intent to start an activity called SecondActivity with the following code:
+
+            Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+
+            // start the activity connect to the specified class
+            startActivity(intent);
+        }
+    });
     }
 }
